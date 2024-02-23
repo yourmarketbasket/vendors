5614d3858e7d6a789cdf32782cfcea22226854bf
codethelabs <codethelabs@gmail.com>
2024-02-23 23:11:36 +0300

    new changes

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -68,212 +68,218 @@ void initState() {
         ],
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Obx(() => Row(
+        child: Container(
+          height: 500,
+          width: 320,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Icon(
-                    loginController.error.value ? Icons.error_outline : Icons.check_circle_outline,
-                    color: loginController.error.value ? AppTheme.dangerColor : AppTheme.mainColor,
+                  Obx(() => Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        loginController.error.value ? Icons.error_outline : Icons.check_circle_outline,
+                        color: loginController.error.value ? AppTheme.dangerColor : AppTheme.mainColor,
+                      ),
+                      Text(
+                        loginController.feedback.value,
+                        style: TextStyle(
+                          color: loginController.error.value  ? AppTheme.dangerColor : AppTheme.mainColor,
+                        ),
+                      ),
+                    ],
+                  )
+          
                   ),
-                  Text(
-                    loginController.feedback.value,
-                    style: TextStyle(
-                      color: loginController.error.value  ? AppTheme.dangerColor : AppTheme.mainColor,
+                  SizedBox(height: 10,),
+                  TextField(
+                    
+                    style: TextStyle(color: Colors.white),
+                    controller: _phone,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hoverColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)
+                      ),
+                      labelText: "Phone Number",
+                      prefixIcon: Icon(Icons.dialpad_rounded),
+                      focusColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)
+                      ),
+                                
+                    ),
+                    
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _password,
+                    obscureText: _obscureText,
+                    style: TextStyle(color: Colors.white),
+                    obscuringCharacter: "x",
+                    decoration: InputDecoration(
+                      focusColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)
+                      ),
+                      border: OutlineInputBorder(),
+                      labelText: "Password",
+                      prefixIcon: Icon(Icons.password_outlined),
+                      suffixIcon: IconButton(
+                        icon: Icon(_obscureText
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      ),
                     ),
                   ),
-                ],
-              )
-
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                
-                style: TextStyle(color: Colors.white),
-                controller: _phone,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hoverColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white)
+                  SizedBox(height: 20),
+                  Stack(                
+                    children: [
+                      Container(
+                        height: 50, // Adjust height as needed
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          boxShadow: [
+                            BoxShadow(
+                              color: AppTheme.mainColor.withOpacity(0.8), // Adjust opacity here
+                              blurRadius: 20,
+                              spreadRadius: 2,
+                              offset: Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: 10,
+                        left: 10,
+                        width: 280,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                Map<String, dynamic> data = {
+                                  "phone":_phone.text,
+                                  "password":_password.text
+                                };
+                                final Map<String, dynamic> response = await loginController.loginUser(context,data);
+                                
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: AppTheme.mainColor,
+                                elevation: 0, // Remove button elevation
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.logout_outlined, color: Colors.white),
+                                  Text("Login", style: TextStyle(color: Colors.white)),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  labelText: "Phone Number",
-                  prefixIcon: Icon(Icons.dialpad_rounded),
-                  focusColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white)
-                  ),
-                            
-                ),
-                
-              ),
-              SizedBox(height: 20),
-              TextField(
-                controller: _password,
-                obscureText: _obscureText,
-                style: TextStyle(color: Colors.white),
-                obscuringCharacter: "x",
-                decoration: InputDecoration(
-                  focusColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white)
-                  ),
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
-                  prefixIcon: Icon(Icons.password_outlined),
-                  suffixIcon: IconButton(
-                    icon: Icon(_obscureText
-                        ? Icons.visibility
-                        : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _obscureText = !_obscureText;
-                      });
-                    },
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Stack(                
-                children: [
+                  SizedBox(height: 20),
                   Container(
-                    height: 50, // Adjust height as needed
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppTheme.mainColor.withOpacity(0.8), // Adjust opacity here
-                          blurRadius: 20,
-                          spreadRadius: 2,
-                          offset: Offset(0, 4),
+                    height: 20,// Ensure the Container has constraints
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 1,
+                          color: Colors.grey,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5.0), // Adjust padding as needed
+                          child: Text("OR", style: TextStyle(color: AppTheme.mainColor, fontSize: 10)),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 1,
+                          color: Colors.grey,
                         ),
                       ],
                     ),
                   ),
-                  Positioned(
-                    top: 10,
-                    left: 10,
-                    width: 280,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(5),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            Map<String, dynamic> data = {
-                              "phone":_phone.text,
-                              "password":_password.text
-                            };
-                            final Map<String, dynamic> response = await loginController.loginUser(context,data);
-                            
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: AppTheme.mainColor,
-                            elevation: 0, // Remove button elevation
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
+                  Stack(                
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 150, // Adjust height as needed
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          boxShadow: [
+                            BoxShadow(
+                              color: AppTheme.dangerColor.withOpacity(0.8), // Adjust opacity here
+                              blurRadius: 20,
+                              spreadRadius: 2,
+                              offset: Offset(0, 4),
                             ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(Icons.logout_outlined, color: Colors.white),
-                              Text("Login", style: TextStyle(color: Colors.white)),
-                            ],
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: 10,
+                        width: 150,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                primary: AppTheme.dangerColor,
+                                elevation: 0, // Remove button elevation
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.person_add_alt, color: Colors.white),
+                                  Text("Register", style: TextStyle(color: Colors.white)),
+                                ],
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
+                  SizedBox(height: 20,),
+                  Text("Forgot Password?", style: TextStyle(fontSize: 10, color: Colors.white),),
+          
+                  SizedBox(height: 20,),
+                  Text("\u00A9"+DateTime.now().year.toString()+" All Rights Reserved", style: TextStyle(fontSize: 10, color: Colors.white),)
+          
+          
+          
+          
+          
+          
                 ],
               ),
-              SizedBox(height: 20),
-              Container(
-                height: 20,// Ensure the Container has constraints
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 1,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5.0), // Adjust padding as needed
-                      child: Text("OR", style: TextStyle(color: AppTheme.mainColor, fontSize: 10)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 1,
-                      color: Colors.grey,
-                    ),
-                  ],
-                ),
-              ),
-              Stack(                
-                children: [
-                  Container(
-                    height: 50,
-                    width: 150, // Adjust height as needed
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppTheme.dangerColor.withOpacity(0.8), // Adjust opacity here
-                          blurRadius: 20,
-                          spreadRadius: 2,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    top: 10,
-                    width: 150,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(5),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            primary: AppTheme.dangerColor,
-                            elevation: 0, // Remove button elevation
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(Icons.person_add_alt, color: Colors.white),
-                              Text("Register", style: TextStyle(color: Colors.white)),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20,),
-              Text("Forgot Password?", style: TextStyle(fontSize: 10, color: Colors.white),),
-
-              SizedBox(height: 20,),
-              Text("\u00A9"+DateTime.now().year.toString()+" All Rights Reserved", style: TextStyle(fontSize: 10, color: Colors.white),)
-
-
-
-
-
-
-            ],
+            ),
           ),
         ),
       ),
